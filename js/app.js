6627ebe6324609a0999a8acb4371c38deee90932
AXG25 <axiever.guejia@gmail.com>
2022-10-21 10:45:00 -0500

    ...

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ let intervalo = null;
 let step = 1;
 const start = () => {
   intervalo = setInterval(function () {
-    carrusel.scrollLeft = carrusel.scrollLeft + step;
-    if (carrusel.scrollLeft === maxScrollLeft) {
-      step = step * -1;
-    } else if (carrusel.scrollLeft === 0) {
-      step = step * -1;
+    carrusel.scrollLeft += step;
+    if (carrusel.scrollLeft == maxScrollLeft) {
+      step *= -1;
+    } else if (carrusel.scrollLeft == 0) {
+      step *= -1;
     }
   }, 10);
 };
