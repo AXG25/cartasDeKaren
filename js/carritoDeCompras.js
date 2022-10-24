6359856b64fa716654da05aa5f16f31d4c2fcb30
AXG25 <axiever.guejia@gmail.com>
2022-10-24 17:28:05 -0500

    ...

--- js/carritoDeCompras.js
+++ js/carritoDeCompras.js
@@ -1,20 +1,64 @@
 const scutomsCollections = document.getElementById("scutomsCollections");
+const verCarrito = document.getElementById("verCarrito");
+const sideCartContainer = document.getElementById("sideCart-container");
+const carrito = [];
 
-let carrito = [];
-
-const mostrarProductos = () => {
 productos.forEach((producto) => {
-    let contenedor = document.createElement("div");
-    contenedor.className = "producto"
-    contenedor.innerHTML = `
+  let contenedor = document.createElement("div");
+  contenedor.className = "producto";
+  contenedor.innerHTML = `
     <img src="${producto.img}">
     <p>${producto.nombre}</p>
     <span>${producto.precio} $</span>
-    <div class="button">
-       <a href="#" class="btn">comprar ahora</a> 
-    </div>
     `;
-    scutomsCollections.append(contenedor)
+
+  scutomsCollections.append(contenedor);
+
+  let btnComprar = document.createElement("div");
+  btnComprar.className = "button";
+  btnComprar.innerHTML = `
+    <a class="btn">agregar a la cesta</a> 
+    `;
+
+  contenedor.append(btnComprar);
+
+  btnComprar.addEventListener("click", () => {
+    carrito.push({
+      id: producto.id,
+      img: producto.img,
+      nombre: producto.nombre,
+      precio: producto.precio,
+    });
+    console.log(carrito);
+  });
 });
-};
-mostrarProductos()
+
+verCarrito.addEventListener("click", () => {
+ sideCartContainer.innerHTML = "";
+ carrito.forEach((producto) => {
+    var productoSideCart = document.createElement("div");
+    productoSideCart.className = "producto-sideCart";
+    productoSideCart.innerHTML = `
+    <img src="${producto.img}">
+    <p>${producto.nombre}</p>
+    <p>${producto.precio} $</p>
+    `;
+
+    sideCartContainer.append(productoSideCart)
+
+    const total = carrito.reduce((acc, item) => acc + item.precio, 0);
+    document.getElementById("precio").innerHTML = `${total} $`;
+ })
+
+
+});
+
+function openCart() {
+    document.getElementById("sideCart").style.width = "300px";
+  }
+  
+function closeCart() {
+    document.getElementById("sideCart").style.width = "0px";
+  }
+
+  
