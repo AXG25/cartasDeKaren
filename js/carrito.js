571ff2b2c6e61942f30acafc021e5506e28d9a6e
AXG25 <axiever.guejia@gmail.com>
2022-11-19 21:28:30 -0500

    solucionando un problema del total del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,70 +1,81 @@
 // funcion para mostrar los productos del carrito
 const pintarCarrito = () => {
-    // limpiamos el html para que no se repitan los productos
-    sideCartContainer.innerHTML = "";
+  // limpiamos el html para que no se repitan los productos
+  sideCartContainer.innerHTML = "";
 
-    // recorremos el array del carrito el cual ha sido modificado por el boton de comprar y creamos los elemntos html para mostrarlos
-    for (let i = 0; i < carrito.length; i++) {
-      let productoSideCart = document.createElement("div");
-      productoSideCart.className = "producto-sideCart";
-      productoSideCart.innerHTML = `
+  // recorremos el array del carrito el cual ha sido modificado por el boton de comprar y creamos los elemntos html para mostrarlos
+  for (let i = 0; i < carrito.length; i++) {
+    let productoSideCart = document.createElement("div");
+    productoSideCart.className = "producto-sideCart";
+    productoSideCart.innerHTML = `
         <img src="${carrito[i].img}">
         <p>${carrito[i].nombre}</p>
         <p>${carrito[i].precio} $</p>
         <p>cant: ${carrito[i].cantidad}</p>
         `;
-        
-        // lo enchuflamos al html LN-39
-      sideCartContainer.append(productoSideCart);
 
-  
-      // creamos el boton de eliminar
-      let btnEliminar = document.createElement("span");
-      btnEliminar.className = "btn-eliminar";
-      btnEliminar.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-      productoSideCart.append(btnEliminar);
+    // lo enchuflamos al html LN-39
+    sideCartContainer.append(productoSideCart);
 
-      // le damos funcionalidad al boton de eliminar
-      btnEliminar.addEventListener("click", eliminarProducto);
+    // creamos el boton de eliminar
+    let btnEliminar = document.createElement("span");
+    btnEliminar.className = "btn-eliminar";
+    btnEliminar.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    productoSideCart.append(btnEliminar);
 
-        // creamos la funcion para eliminar solo el producto que se le dio click
-        function eliminarProducto() {
-            carrito.splice(i, 1);
-            cantidadCarrito();
-            guardarCarrito();
-            pintarCarrito();
-        }
+    // le damos funcionalidad al boton de eliminar
+    btnEliminar.addEventListener("click", eliminarProducto);
+
+    // funcion que nos muestra el total de la compra
+    const total = carrito.reduce(
+       (acc, item) => acc + item.precio * item.cantidad,
+      0
+     );
+
+    
     
 
+    let totalCompra = document.createElement("div");
+    totalCompra.className = "total-compra";
+    totalCompra.innerHTML = `
+        <p>Total a pagar: <span>${total} $</span></p>
+        `;
+    sideCartContainer.append(totalCompra);
   
-      // funcion que nos muestra el total de la compra
-      const total = carrito.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
-      
-      // lo mostramos en el html
-      document.getElementById("precio").innerHTML = `${total} $`;
-    };
-  };
-  
-  // funcion que abre el carrito
-  function openCart() {
-    document.getElementById("sideCart").style.width = "360px";
+
+    // creamos la funcion para eliminar solo el producto que se le dio click
+    function eliminarProducto() {
+      carrito.splice(i, 1);
+      cantidadCarrito();
+      guardarCarrito();
+      pintarCarrito();
+    }
   }
-  
-  // funcion que cierra el carrito
-  function closeCart() {
-    document.getElementById("sideCart").style.width = "0px";
-  }
-  
-  verCarrito.addEventListener("click", pintarCarrito)
+};
 
-  const cantidadCarrito = () => {
-    const cantidadDeProductos = carrito.length;
-    //guardamos la cantidad de productos en el local storage
-    localStorage.setItem("cantidadDeProductos", JSON.stringify(cantidadDeProductos));
-    //obtenemos la cantidad de productos del local storage y la mostramos en el html
-    contadorCarrito.innerText = JSON.parse(localStorage.getItem("cantidadDeProductos"));
-  };
+// funcion que abre el carrito
+function openCart() {
+  document.getElementById("sideCart").style.width = "360px";
+}
 
-  cantidadCarrito()
+// funcion que cierra el carrito
+function closeCart() {
+  document.getElementById("sideCart").style.width = "0px";
+}
 
-           
+verCarrito.addEventListener("click", pintarCarrito);
+
+const cantidadCarrito = () => {
+  const cantidadDeProductos = carrito.length;
+  //guardamos la cantidad de productos en el local storage
+  localStorage.setItem(
+    "cantidadDeProductos",
+    JSON.stringify(cantidadDeProductos)
+  );
+  //obtenemos la cantidad de productos del local storage y la mostramos en el html
+  contadorCarrito.innerText = JSON.parse(
+    localStorage.getItem("cantidadDeProductos")
+  );
+};
+
+cantidadCarrito();
